1bce1a44c96bcfd212d82d0fc704f34f2cbdbbc5
Pedro Foschini <plfoschini@gmail.com>
2022-01-17 21:37:42 -0300

    Últimas aulas do curso

--- p11-CalculaPoupanca2/Program.cs
+++ p11-CalculaPoupanca2/Program.cs
@@ -4,23 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _10_CalculaPoupanca
+namespace p11_CalculaPoupanca2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Executando projeto 10 - Calcula Poupança");
+            Console.WriteLine("Executando projeto 11 - Calcula Poupança 2");
 
             double valorInvestido = 1000.0;
 
-            int contadorMes = 1;
-            while (contadorMes <= 12){
-
-                // 0.36% = 0.0036
-                valorInvestido += valorInvestido * 0.0036;
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
                 Console.WriteLine("Após " + contadorMes + "meses, você terá R$" + valorInvestido);
-                contadorMes++;
             }
 
 
---
+++ p13-ForEncadeado/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p13_ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 13 - Laços encadeados");
+
+            // Usando break
+            for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if(contadorColuna >= contadorLinha)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+            // Forma alternativa
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for (int contadorColuna = 0; contadorColuna <= contadorLinha; contadorColuna++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Fim da execução. Pressione Enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
